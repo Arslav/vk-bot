d5c670e729f743657ea985c6cd34e41c6a3440a3
Vyacheslav Ardesov <ardesov.v@red-promo.ru>
2021-04-04 01:56:14 +0500

    Исправление конфигов

--- bot.php
+++ bot.php
@@ -6,15 +6,16 @@ use Monolog\Handler\StreamHandler;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$log = new Logger('vk bot');
-$log->pushHandler(new StreamHandler('app.log', Logger::INFO));
-
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+define('LOG_LEVEL', $_ENV['LOG_LEVEL']);
 define('VK_API_TOKEN', $_ENV['VK_API_TOKEN']);
 define('VK_API_VERSION', $_ENV['VK_API_VERSION']);
 define('VK_API_CONFIRM_STRING', $_ENV['VK_API_CONFIRM_STRING']);
+
+$log = new Logger('bot');
+$log->pushHandler(new StreamHandler('app.log', LOG_LEVEL));
 
 $vk = vk_api::create(VK_API_TOKEN, VK_API_VERSION)->setConfirm(VK_API_CONFIRM_STRING);
 
