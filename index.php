ed364f4f7f8a55007cb887c36a82c8d172292ef1
Vyacheslav Ardesov <ardesov.v@red-promo.ru>
2021-05-07 13:43:22 +0500

    Доработка регулярных выражений для команд

--- index.php
+++ index.php
@@ -14,11 +14,12 @@ $app = App::init($container);
 $app->add(new AutistCommand([
     '^ыы+$',
     '^кря+$',
+    '^ря+$',
     '^[аы]{2,}$',
 ]));
 $app->add(new WhoAmICommand(['^сла+ва+(\,)? [кчх]то+ я\s?(\?)*$']));
 $app->add(new WhoWeCommand(['^сла+ва+(\,)? [хк]то+ мы+\s?(\?)*$']));
-$app->add(new GiveMePillsCommand(['сла+ва+(\,)? дай таблетки+\s?[(\!)(\?)]*$']));
+$app->add(new GiveMePillsCommand(['сла+ва+(\,)? дай табле(тки+|то+к)\s?[(\!)(\?)]*$']));
 $app->add(new PressFCommand(['(.*\s|^)f([\.\!\?\,]|\s.*|$)']));
 
 $app->run();
